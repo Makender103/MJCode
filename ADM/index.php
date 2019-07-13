3387e75def993c6287d9e831b7531e66437ac407
makender103 <makender103@gmail.com>
2019-07-13 14:36:12 -0300

    add

--- ADM/index.php
+++ ADM/index.php
@@ -6,50 +6,63 @@ $query = "SELECT id, name, email, message, service FROM scinnob_Contact";
 $response = mysqli_query($connect,$query);
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>SCINNOB - ADM</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+
+
+
+  <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/fixed.css">
+
+
+  <title>SCINNOB - Blog</title>
+  <link rel="icon" href="img/logo.png">
+
 </head>
 <body>
+  <div container="">
+        <nav>
+          <div>
+             <h3 class="text-center">LIST OF CONTACT</h3>
+          </div>
+        </nav>
 
-<div class="container">
-  <p class="text-center">LIST OF CONTACT</p>            
-  <table class="table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Email</th>
-         <th>Service</th>
-        <th>Message</th>
-        <th>Option</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php foreach ($response as $key => $value) { ?>
-      <tr>
-        <td><?php echo $value['id']?></td>
-        <td><?php echo $value['name']?></td>
-        <td><?php echo $value['email']?></td>
-        <td><?php echo $value['service']?>Web</td>
-        <td><?php echo $value['message']?></td>
-        <th>
-          <a href="#" class="btn btn-info btn-lg">
-          <span class="glyphicon glyphicon-pencil"></span> Answer
-        </a>
-        </th>
-      </tr>
-    <?php }?>
-    
-    </tbody>
-  </table>
+        <table class="table">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">NAME</th>
+            <th scope="col">EMAIL</th>
+            <th scope="col">SERVICE</th>
+             <th scope="col">MESSAGE</th>
+            <th scope="col">OPTION</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($response as $key => $value) { ?>
+            <tr>
+              <td><?php echo $value['id']?></td>
+              <td><?php echo $value['name']?></td>
+              <td><?php echo $value['email']?></td>
+              <td><?php echo $value['service']?>Web</td>
+              <td><?php echo $value['message']?></td>
+              <td>
+                <ion-icon data-toggle="tooltip" name="create" data-placement="right">Answer</ion-icon>
+              </td>
+            </tr>
+          <?php }?>
+        </tbody>
+      </table>
 </div>
 
 </body>
