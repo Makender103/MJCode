ceb59e3c6004519f4fe4095a059922d5a5857279
makender103 <makender103@gmail.com>
2019-08-11 11:42:57 -0300

    sign up

---
+++ ADM/sign-up.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/fixed.css">
+
+
+	<title>MJTECH - ADM login Form</title>
+	<link rel="icon" href="img/logo.png">
+
+
+</head>
+
+
+    <body class="bck_log">
+        <div class="container-fluid bg">
+        <div class="row">
+
+
+        <div class="col-md-3 col-sm-3 col-xs-12"></div>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+
+                        <form id="log" action="" method="post" enctype="multipart/form-data">
+                        <h1>MJTEC - Sign Up</h1>
+                        <img class="card-img-top img img-responsive" src="img/t4.gif">
+
+                          <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" class="form-control" placeholder="Enter your Name" name="Name">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Email</label>
+                            <input type="email" class="form-control" placeholder="Enter your email" name="email">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Username</label>
+                            <input type="text" class="form-control" placeholder="Enter your Username" name="username">
+                          </div>
+
+                          <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" placeholder="Create your Password" name="password">
+                          </div>
+                          <button class="btn btn-danger btn-block" type="submit" name="Sign Up">Sign Up</button>
+
+
+                        </form>
+
+        </div>
+
+
+        </div>
+        </div>
+
+    </body>
+</html>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
