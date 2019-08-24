11d6d8917a7c0aa60c566ee18110af1daadd9966
Your Name <you@example.com>
2019-08-24 20:32:03 -0300

    Change Name

--- Includes/connection.php
+++ Includes/connection.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $senha = "";
-$database = "MJTECH";
+$database = "MJCode";
 $connect = mysqli_connect($host, $user, $senha, $database) or
 die("Error to connect.");
 mysqli_query($connect, "SET NAMES 'utf8'");
