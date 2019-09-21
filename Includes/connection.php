54b92d8d120e35850ffe583eb3dd07a13a2bbe0c
Marc kender R. Jean Charles <makender103@gmail.com>
2019-09-21 13:03:24 -0300

    Update connection.php

--- Includes/connection.php
+++ Includes/connection.php
@@ -1,8 +1,8 @@
 <?php
-$host = "host2";
-$user = "mjcode";
-$senha = "@M&Jcod&16";
-$database = "MJcode";
+$host = "localhost";
+$user = "mjcodene_mjcode";
+$senha = "0061995@xp";
+$database = "mjcodene_MJcode";
 $connect = mysqli_connect($host, $user, $senha, $database) or
 die("Error to connect.");
 mysqli_query($connect, "SET NAMES 'utf8'");
